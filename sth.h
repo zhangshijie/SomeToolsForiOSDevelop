d47fcd61c39ef1ec5346997a3160676929ad4e79
 zhangshijie <cjzhang88@gmail.com>
2013-10-19 09:56:00 +0800

    sth

---
+++ sth.h
@@ -0,0 +1,13 @@
+//
+//  sth.h
+//  SomeToolsForiOSDevelop
+//
+//  Created by 张 世杰 on 13-10-19.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface sth : NSObject
+
+@end
---
+++ sth.m
@@ -0,0 +1,13 @@
+//
+//  sth.m
+//  SomeToolsForiOSDevelop
+//
+//  Created by 张 世杰 on 13-10-19.
+//
+//
+
+#import "sth.h"
+
+@implementation sth
+
+@end
